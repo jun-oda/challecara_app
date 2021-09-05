6b9637acad56ed164fddc19d5d35149036766520
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-09-05 17:14:54 +0900

    一旦コミット1

---
+++ app/controllers/calendars_controller.rb
@@ -0,0 +1,44 @@
+class CalendarsController < ApplicationController
+    def index
+        @calendars = Calendar.all
+      end
+    
+      def new
+        @calendar = Calendar.new
+      end
+    
+      def show
+        @calendar = Calendar.find(params[:id])
+      end
+    
+      def create
+        Calendar.create(calendar_parameter)
+        redirect_to calendars_path
+      end
+    
+      def destroy
+        @calendar = Calendar.find(params[:id])
+        @calendar.destroy
+        redirect_to calendars_path, notice:"削除しました"
+      end
+    
+      def edit
+        @calendar = Calendar.find(params[:id])
+      end
+    
+      def update
+        @calendar = Calendar.find(params[:id])
+        if @calendar.update(calendar_parameter)
+          redirect_to calendars_path, notice: "編集しました"
+        else
+          render 'edit'
+        end
+      end
+    
+      private
+    
+      def calendar_parameter
+        params.require(:calendar).permit(:title, :content, :start_time)
+      end
+    
+end
