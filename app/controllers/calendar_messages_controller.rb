decbfcfcdff60da983d54d73fa4eda3a021605e7
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-10-15 15:40:55 +0900

    indexアクション完成?

--- app/controllers/calendar_messages_controller.rb
+++ app/controllers/calendar_messages_controller.rb
@@ -1,14 +1,24 @@
 class CalendarMessagesController < ApplicationController
   def index
+    @messages = CalendarMessage.find_by(calendar_id: params[:id])
+    @message = CalendarMessage.new
   end
 
   def create
+    @message = CalendarMessage.new(calendar_message_params)
+
+    if @message.save
+      redirect_to calendar_messages_path
+    else
+      render calendar_messages_path
+    end
   end
 
   def destroy
   end
 
-  def calendar_message_parameter
-    params.require(:calendar_message).permit(:message, :user_id, :created_at, :calendar_id)
+  def calendar_message_params
+    params.require(:calendar_message).permit(:message)
+    .merge(user_id: current_user.id, calendar_id: params[:id])
   end
 end
--- app/models/calendar_message.rb
+++ app/models/calendar_message.rb
@@ -1,4 +1,5 @@
 class CalendarMessage < ApplicationRecord
+  validates :message, presence: true
   belongs_to :user
   belongs_to :calendar
 end
