cda614f81d6cd5e1974dc65e9fc26579cdd3fb69
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-10-28 08:28:42 +0900

    createアクション完成

--- app/controllers/calendar_messages_controller.rb
+++ app/controllers/calendar_messages_controller.rb
@@ -1,16 +1,13 @@
 class CalendarMessagesController < ApplicationController
   def index
+    #@calendar = Calendar.find(params[:id])
     @messages = CalendarMessage.find_by(calendar_id: params[:id])
+    @calendar_message = CalendarMessage.new
   end
 
   def create
-    @message = CalendarMessage.new(calendar_message_params)
-
-    if @message.save
-      redirect_to calendar_calendar_messages_path
-    else
-      render calendar_calendar_messages_path
-    end
+    @calendar_message = CalendarMessage.create(calendar_message_params)
+    redirect_to calendar_calendar_messages_path
   end
 
   def destroy
@@ -18,6 +15,6 @@ class CalendarMessagesController < ApplicationController
 
   def calendar_message_params
     params.require(:calendar_message).permit(:message)
-.merge(user_id: current_user.id, calendar_id: params[:id])
+    .merge(user_id: current_user.id, calendar_id: params[:calendar_id])
   end
 end
