7446d0f2ba73eae9c8b2547f6eef69c0104ab648
momod <momo.dec1201@gmail.com>
2021-11-04 18:26:13 +0900

    とりあえずmessage表示

--- app/controllers/calendar_messages_controller.rb
+++ app/controllers/calendar_messages_controller.rb
@@ -2,11 +2,15 @@ class CalendarMessagesController < ApplicationController
   def index
     #@calendar = Calendar.find(params[:id])
     @messages = CalendarMessage.find_by(calendar_id: params[:id])
+    #↑userとcalendar_idが一致するCalendarMessageモデルが返ってくる？
     @calendar_message = CalendarMessage.new
+    #message保存用のdb（CalendarMessageモデル）新規作成？
+    @allmessages = CalendarMessage.select("message")
   end
 
   def create
     @calendar_message = CalendarMessage.create(calendar_message_params)
+  #任意のmessagedb（CalendarMessageモデル）にmessageを追加？
     redirect_to calendar_calendar_messages_path
   end
 
