0bf2aca13ed456a851c997fa2f4e3f6ad5dd44f3
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-11-16 10:54:18 +0900

    メッセージ削除追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,6 @@ class Group < ApplicationRecord
   has_many :invites
   has_many :group_users
   has_many :users, through: :group_users
-  has_many :calendars
+  has_many :calendars, dependent: :destroy
   #accepts_nested_attributes_for :group_users
 end
