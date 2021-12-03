ad07c2e0fc8cdef8cae51b805eee047ab178cff9
jun-oda <75347980+jun-oda@users.noreply.github.com>
2021-12-03 17:15:07 +0900

    save

--- vendor/cache/ruby/2.7.0/gems/stimulus-rails-0.6.0/lib/stimulus/engine.rb
+++
@@ -1,9 +0,0 @@
-module Stimulus
-  class Engine < ::Rails::Engine
-    initializer "stimulus.assets" do
-      if Rails.application.config.respond_to?(:assets)
-        Rails.application.config.assets.precompile += %w( stimulus )
-      end
-    end
-  end
-end
--- vendor/cache/ruby/2.7.0/gems/turbo-rails-0.8.1/lib/turbo/version.rb
+++
@@ -1,3 +0,0 @@
-module Turbo
-  VERSION = "0.8.1"
-end
